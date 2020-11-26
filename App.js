ec31227ae56e250b4f892381bca6d10e5f30b505
hdus.vn <phamductbcm@gmail.com>
2020-11-26 22:44:03 +0700

    login page

--- App.js
+++ App.js
@@ -1,46 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, Image, TextInput } from 'react-native';
+//This is an example code for Navigator// 
+import React, { Component } from 'react';
+//import react in our code. 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={{ backgroundColor: '#3e86fa', width: '100%', height: '40px' }}>
-      </View>
-      <Image style={{ width: '100px', height: '100px', paddingBottom: 20 }} source={require('./image/react-native-logo.79778b9e.png')} />
-      <Text style={styles.logo}>Login</Text>
-      <StatusBar style="auto" />
-      {input("User Name")}
-      {input("Password")}
-      <Button style={{ color: "#3e86fa" }} title="Login" onPress={() => Alert.alert('Simple Button pressed')} />
-    </View>   
-  );
-}
+//Import react-navigation
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
 
-function input(label){
-    return (
-      <View style={{ flexDirection: 'row', alignItems: 'flex-end|center', width: '70%', paddingBottom: 20 }}>
-        <View style={{ width: '30%' }}>
-          <Text style={styles.text} >{label}</Text>
-        </View>
-        <TextInput style={{color: 'white', height: 40, borderColor: 'gray', borderWidth: 1, width:'70%', height: 20 }} onChangeText={text => { }} />
-      </View>
-    );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    backgroundColor: '#01072e',
+import Login from './Page/Login';
+import Wellcome from './Page/Wellcome';
+//import all the screens we are going to switch 
+const App = createStackNavigator({
+  //Constant which holds all the screens like index of any book 
+    Login: { screen: Login }, 
+    //First entry by default be our first screen if we do not define initialRouteName
+    Wellcome: { screen: Wellcome }, 
   },
-  text: {
-    color: '#fff',
-    fontWeight: 600
-  },
-  logo:{
-    color: '#fff',
-    fontSize: 30
+  {
+    initialRouteName: 'Login',
   }
-});
+);
+export default createAppContainer(App);
---
+++ Page/Wellcome.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert, Image, TextInput } from 'react-native';
+
+export default function Wellcome() {
+  return (
+    <View>
+        Wellcome
+    </View>   
+  );
+}
