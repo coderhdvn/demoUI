0a27f397eddc6e97ddee432cbd12298ab4768c01
hdus.vn <phamductbcm@gmail.com>
2020-12-29 21:40:56 +0700

    edit notification

--- page/notification.js
+++ page/notification.js
@@ -1,10 +1,24 @@
-import { View } from "native-base";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+
 import React from 'react';
 
 export default class NotificationPage extends React.Component {
     render(){
-        return (<View style={{backgroundColor:"#6FF6FF", height:'100%', width:'100%'}}>
+        return (
+            <View style={styles.container}>
+                <View style={styles.header} />
+                <View>
+                    <TouchableOpacity onPress={()=>{}} style={styles.item}> 
+                    </TouchableOpacity>
+                </View>
+            </View>
 
-        </View>)
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {flex:1},
+    header: { backgroundColor: "#6FF6FF", height: '10%', width: '100%' },
+    item: { height:100, borderColor: "#000", borderWidth: 1}
+})
