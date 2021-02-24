3b84a47373e6e9d06f44ff48bbed3054eb90a115
AnhTu <nguyenanhtu1999@gmail.com>
2021-02-24 23:18:34 +0300

    design detailInfo and update Scanpage

--- page/detailInfo.js
+++ page/detailInfo.js
@@ -1,51 +1,84 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 
 export default class DetailInfo extends React.Component {
-  
-  constructor(props) {
-    super(props)
-  }
+    
+    state = {
+      name: "Iphone X",
+      producer: 'Apple',
+      expiry_day: '02/02/2020',
+      date_of_manufacture: '02/02/2020',
+      serial_number: '123456789',
+      distributor: ['distributor 1', 'distributor 2', 'distributor 3', 'distributor 4'],
+      description: 'Ram 12GB - ROM 64GB',
+      image: 'https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-x-new-1.jpg'
+    }
 
   render(){
     return (
       <View style={styles.container}>
-            <Text style={{ color:"#FFF", fontSize:25, width: "100%", textAlign: "center", fontWeight: "300", padding: 10}}>THÔNG TIN SẢN PHẨM</Text>
-            <ScrollView style={styles.scrollView}>
-            <View>
+        <View style={styles.shadow}>
+          <Text style={styles.title}>THÔNG TIN SẢN PHẨM</Text>
+        </View>
+        <View style={styles.titleView}>
+          <Text style={styles.title}>THÔNG TIN SẢN PHẨM</Text>
+        </View>
+        
+        <View style={styles.contentView}>
+            <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+            <View style={styles.item}>
             <Text style={styles.textTitle}> * Tên sản phẩm:</Text> 
-            <Text style={styles.textContent}> {this.props.route.params.data.title} </Text>
+            {/* <Text style={styles.textContent}> {this.props.route.params.data.title} </Text> */}
+            <Text style={styles.textContent}>{this.state.name}</Text>
             </View>
-            <View>
-            <Text style={styles.textTitle}> * Nhà sản phẩm:</Text> 
-            <Text style={styles.textContent}> {this.props.route.params.data.description}</Text>
+            <View style={styles.item}>
+            <Text style={styles.textTitle}> * Nhà sản xuất:</Text> 
+            {/* <Text style={styles.textContent}> {this.props.route.params.data.description}</Text> */}
+            <Text style={styles.textContent}>{this.state.producer}</Text>
             </View>
-            <View>
+            <View style={styles.item}>
             <Text style={styles.textTitle}> * Ngày sản xuất:</Text> 
-            <Text style={styles.textContent}> 2/7/2020</Text>
+            <Text style={styles.textContent}>{this.state.date_of_manufacture}</Text>
             </View>
-            <View>
+            <View style={styles.item}>
             <Text style={styles.textTitle}> * Hạn sử dụng:</Text> 
-            <Text style={styles.textContent}> {this.props.route.params.data.title}</Text>
+            {/* <Text style={styles.textContent}> {this.props.route.params.data.title}</Text> */}
+            <Text style={styles.textContent}>{this.state.expiry_day}</Text>
             </View>
-            <View>
+            <View style={styles.item}>
             <Text style={styles.textTitle}> * Serial number:</Text> 
-            <Text style={styles.textContent}> 123456</Text>
+            <Text style={styles.textContent}>{this.state.serial_number}</Text>
             </View>
-            <View>
+            <View style={styles.item}>
             <Text style={styles.textTitle}> * Nhà phân phối:</Text> 
-            <Text style={styles.textContent}> Cellphone S</Text>
-            </View>
-            <View>
-            <Text style={styles.textTitle}> * Mô tả:</Text> 
-            <Text style={styles.textContent}> Ram 12GB - ROM 64GB</Text>
+            <FlatList 
+              showsHorizontalScrollIndicator={false}
+              horizontal={true}
+              data={this.state.distributor}
+              keyExtractor={(data) => data}
+              renderItem={({item}) => {
+                return (
+                  <View style={styles.listView}>
+                    <Text style={styles.textContent}>{item}</Text>
+                  </View>
+                )
+              }}
+            />
             </View>
 
-            
+            <View style={styles.item}>
+              <Text style={styles.textTitle}> * Mô tả:</Text> 
+              <Text style={styles.textContent}>{this.state.description}</Text>
+            </View>
+
+            <View style={styles.item}>
+              <Text style={styles.textTitle}> * Hình ảnh:</Text> 
+              <Image style={styles.image} source={{uri: this.state.image}} />
+            </View>
         </ScrollView>
         
-        
+        </View>
       </View>
     );
   }
@@ -56,31 +89,80 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#00F0FF",
 },
+  titleView: {
+    marginLeft: 50,
+    marginRight: 40,
+    marginBottom: 10,
+    marginTop: 20,
+    backgroundColor: 'white',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.2,
+    elevation: 20,
+    position: 'absolute',
+    borderRadius: 2
+  },
+  shadow: {
+    marginLeft: 40,
+    marginRight: 50,
+    marginBottom: 20,
+    marginTop: 30,
+    backgroundColor: '#777777',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.2,
+    elevation: 20,
+    borderRadius: 5
+  },
   title: {
-      margin: 20,
-      borderWidth: 2,
+      color:"black", 
+      fontSize:25, 
+      width: "100%", 
+      textAlign: "center", 
+      fontWeight: "300", 
+      padding: 10,
   },
   titleContent: {
     fontSize: 22,
   },
-  logo:{
-    justifyContent: "center",
-    alignItems: "center",
-    resizeMode: "cover"
-  },
   textTitle: {
       fontSize: 15,
       color: "#6A6464",
-      paddingLeft: 20
+      marginTop: 10
     },
   textContent: {
     fontSize: 20,
-    paddingBottom: 10,
-    paddingLeft: 20
-    },
-    scrollView: {
-        backgroundColor: "#fff",
-        borderRadius: 15,
-        paddingTop: 20
-      },
+  },
+  item: {
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  contentView: {
+    backgroundColor: "#fff",
+    borderTopRightRadius: 30,
+    flex: 1,
+  },
+  scrollView: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  listView: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: 'gray',
+    marginRight: 10
+  },
+  image: {
+    height: 300,
+    width: 300,
+    marginTop: 5
+  }
 });
